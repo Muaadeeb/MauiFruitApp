a7082e7a338325b996a5955b5d5f450dde6ffac1
Timothy Ingledue <timothy.ingledue@orion.com>
2023-03-10 10:24:08 -0600

    ListView - Fruit List display

--- MauiFruitApp/MauiFruitApp/MainPage.xaml.cs
+++ MauiFruitApp/MauiFruitApp/MainPage.xaml.cs
@@ -2,15 +2,25 @@
 
 public partial class MainPage : ContentPage
 {
+    private readonly List<string> fruits = new List<string>()
+    {
+        "Apple", "Banana", "Coconut", "Grape", "Guava", "Kiwi", "Mango", "Melon"
+    };
+
+
 	public MainPage()
 	{
 		InitializeComponent();
+        Fruits.ItemsSource = fruits;
 	}
 
-    private void BtnGoToPage2_Clicked(object sender, EventArgs e)
-    {
-        Navigation.PushAsync(new Page2(EnterUserName.Text));
-    }
+
+
+
+    //private void BtnGoToPage2_Clicked(object sender, EventArgs e)
+    //{
+    //    Navigation.PushAsync(new Page2(EnterUserName.Text));
+    //}
 
     //private void BtnMoveToPage2_Clicked(object sender, EventArgs e)
     //{
